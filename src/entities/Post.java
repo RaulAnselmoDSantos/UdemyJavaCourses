1d82962129b55181b64a6e1ddadb61ddd0b018ea
Raul Anselmo D Santos <raul.trabalho5511@gmail.com>
2026-03-24 02:12:07 -0300

    feat: new execise

---
+++ src/entities/Post.java
@@ -0,0 +1,50 @@
+package entities;
+
+import java.util.Date;
+
+public class Post {
+    private Date date;
+    private String title;
+    private String content;
+    private Integer likes;
+
+    public Post(){}
+    public Post(Date date, String title, String content, Integer likes) {
+        this.date = date;
+        this.title = title;
+        this.content = content;
+        this.likes = likes;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Integer getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Integer likes) {
+        this.likes = likes;
+    }
+}
