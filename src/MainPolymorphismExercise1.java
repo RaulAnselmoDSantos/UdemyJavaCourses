94071ca878190ac173db5a18213c152e29860ffe
Raul Anselmo D Santos <raul.trabalho5511@gmail.com>
2026-04-11 19:37:12 -0300

    fix: the for loop and the colect of the double varivel

--- src/MainPolymorphismExercise1.java
+++ src/MainPolymorphismExercise1.java
@@ -3,53 +3,43 @@ import entities.OutsoircedEmployee;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class MainPolymorphismExercise1 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
-        List<Employee> employee = new ArrayList<>();
+        Locale.setDefault(Locale.US);
+        List<Employee> list = new ArrayList<>();
 
         System.out.print("Enter the number of employs: ");
         int n = sc.nextInt();
-        for (int i = 1; i <= n; i++){
+        for (int i = 0; i < n; i++){
+            System.out.println("Employee #" + (i+1) + " data: ");
             System.out.print("Outsourced (y/n): ");
-            String ch = sc.next();
-            if(ch.equals("y")){
-                System.out.println("Employee #" + i + "data: ");
-                sc.nextLine();
-                System.out.print("Name: ");
-                String name = sc.nextLine();
-                System.out.print("Hours: ");
-                Integer hours = sc.nextInt();
-                System.out.print("Value Per Hour: ");
-                Double vHour = sc.nextDouble();
+            char ch = sc.next().charAt(0);
+            sc.nextLine();
+            System.out.print("Name: ");
+            String name = sc.nextLine();
+            System.out.print("Hours: ");
+            Integer hours = sc.nextInt();
+            System.out.print("Value Per Hour: ");
+            sc.nextLine();
+            Double vHour = sc.nextDouble();
+            if(ch == 'y'){
                 System.out.print("Additional Charge: ");
                 Double charge = sc.nextDouble();
                 Employee ep = new OutsoircedEmployee(name, hours, vHour, charge);
-                employee.add(ep);
+                list.add(ep);
             }
-            if(ch.equals("n")){
-                System.out.println("Employee #" + i + "data: ");
-                sc.nextLine();
-                System.out.print("Name: ");
-                String name = sc.nextLine();
-                System.out.print("Hours: ");
-                Integer hours = sc.nextInt();
-                System.out.print("Value Per Hour: ");
-                sc.nextLine();
-                Double vHour = sc.nextDouble();
+            if(ch == 'n'){
                 Employee ep = new Employee(name, hours, vHour);
-                employee.add(ep);
-                System.out.println("Employee #" + i + " Created");
-            }else {
-                System.out.println("Invalid input");
-                break;
+                list.add(ep);
+                System.out.println("Employee #" + (i+1) + " Created");
             }
         }
         System.out.println("Payments: ");
-        for(Employee ep : employee){
+        for(Employee ep : list){
             System.out.println(ep.getName() + " - $" + ep.payment());
         }
         sc.close();
