c56a3eb45318efce6a072e0c08718b0aeb4f2482
RaulAnselmoDSantos <raul.trabalho5511@gmail.com>
2026-06-10 10:11:28 -0300

    feat: new lesson

---
+++ src/MainPastas.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class MainPastas {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the folder path: ");
+        String path = sc.nextLine();
+
+
+    }
+}
